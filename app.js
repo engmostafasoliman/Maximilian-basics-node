45101d8bb57c5ab86ba51ac959fb599dc7bf802f
Administrator <engmostafasoliman0@gmail.com>
2026-07-14 17:25:53 +0400

    first commit for maximilian course

---
+++ app.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    res.setHeader("Content-Type", "text/html");
+    url = req.url;
+    method = req.method;
+    if (url === "/") {
+        res.write("<html>");
+        res.write("<head><title>Enter message </title></head>");
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>");
+        res.write("</html>");
+        return res.end();
+    }
+    if (url === "/message" && req.method === "POST") {
+        const body = [];
+        req.on("data", (data) => {
+            console.log(data);
+            body.push(data);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync("message.txt", message);
+        })
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();   
+    }
+    res.statusCode = 200;
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Hello first node js server</h1></body>");
+    res.write("</html>");
+
+    res.end();
+});
+
+server.listen(3001); 
