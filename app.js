7f1fef1141592fd2b41dc24165b72665c018ebed
Administrator <engmostafasoliman0@gmail.com>
2026-07-20 10:36:54 +0400

    refactor: update middleware logging and response messages in app.js

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-    console.log("In the middleware");
-    next();
+    console.log("In the middleware from first middleware");
+    res.send("<h1>Hello from Express from first middleware!</h1>");
+    //next();
 });
 
 app.use((req, res, next) => {
-    console.log("In another middleware");
-    res.send("<h1>Hello from Express!</h1>");
+    console.log("In another middleware from second middleware");
+    res.send("<h1>Hello from Express from second middleware!</h1>");
 });
 
 const server = http.createServer(app);
