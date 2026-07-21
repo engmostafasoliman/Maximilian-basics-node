7ad17c4cd7f3b8ccd3c0e802e9100768477728fd
Administrator <engmostafasoliman0@gmail.com>
2026-07-21 18:00:53 +0400

    chore: install project dependencies and generate node_modules directory

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router= express.Router();
 const path = require("path");
 const rootDir = require("../util/paths");
+const adminData = require("./admin");
 router.get("/",(req, res, next) => {
     
-    console.log("In the middleware from last middleware");
-res.sendFile( path.join(rootDir,"views","shop.html"));
+    const products = adminData.products;
+res.render("shop",{prods:products,docTitle:"Shop"});
 });
 
 
