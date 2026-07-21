0f19fb0b3afc7ac75ee97f88d43e0ad55014c89e
Administrator <engmostafasoliman0@gmail.com>
2026-07-21 16:17:44 +0400

    feat: serve static assets and implement UI views with styling for shop, add-product, and 404 pages

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const path = require("path");
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,"public")));
 
     
 
