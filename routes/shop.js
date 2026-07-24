4b0dd898f3710eb36098ed1356c8d6f963de3639
Administrator <engmostafasoliman0@gmail.com>
2026-07-24 16:38:47 +0400

    refactor: implement MVC architecture by creating product models, controllers, and modularized routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const router= express.Router();
-const path = require("path");
-const rootDir = require("../util/paths");
-const adminData = require("./admin");
-router.get("/",(req, res, next) => {
-    
-    const products = adminData.products;
-res.render("shop",{prods:products,pageTitle:"Shop",path:"/",});
-});
+const productsController = require("../controllers/products");
+router.get("/",productsController.getProducts);
 
 
 module.exports = router;
