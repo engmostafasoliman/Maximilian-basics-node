bbf9ad34f00c8228f0ff6c1ee7f1108ca7e41c2d
Administrator <engmostafasoliman0@gmail.com>
2026-07-24 10:55:56 +0400

    refactor: migrate views to EJS and update app configuration to use EJS engine

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+//const expressHbs = require("express-handlebars");
 const app = express();
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const path = require("path");
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,"public")));
-app.engine("hbs",expressHbs({layoutsDir:"views/layouts/",defaultLayout:"main-layout",extname:"hbs"}));
-app.set("view engine","hbs");
+//app.engine("hbs",expressHbs({layoutsDir:"views/layouts/",defaultLayout:"main-layout",extname:"hbs"}));
+app.set("view engine","ejs");
 app.set("views","views");
     
 
@@ -17,7 +17,7 @@ app.use(shopRoutes);
 
 
 app.use((req, res, next) => {
-    res.status(404).render("404",{pageTitle:"Not Found",layout:"main-layout",productCss:false,formCss:false,errorCss:true,shopActive:false,addProductActive:false});
+    res.status(404).render("404",{pageTitle:"Not Found",path:"/"});
 });
 app.listen(3001); 
 module.exports = app;
