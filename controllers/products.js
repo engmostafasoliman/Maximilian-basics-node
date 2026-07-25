75f3e26b4f081ac5a3502369fbb26f20bbe990dc
Mostafa Soliman <engmostafasoliman@gmail.com>
2026-07-25 19:59:46 +0400

    refactor: restructure view directory into admin and shop modules and update controller rendering paths

--- controllers/products.js
+++ controllers/products.js
@@ -2,8 +2,8 @@
 const Product = require("../models/products");
 
 exports.getAddProduct = (req, res, next) => {
-    
-   res.status(200).render("add-product",{pageTitle:"Add Product",path:"/admin/add-product"});
+
+    res.status(200).render("admin/add-product", { pageTitle: "Add Product", path: "/admin/add-product" });
 
 }
 
@@ -13,8 +13,8 @@ exports.postAddProduct = (req, res, next) => {
     res.redirect("/");
 }
 
-exports.getProducts = (req, res, next) => {  
-    Product.fetchAll((products)=>{
-        res.render("shop",{prods:products,pageTitle:"Shop",path:"/",});
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render("shop/products-list", { prods: products, pageTitle: "Shop", path: "/", });
     });
 }
--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const router= express.Router();
+const router = express.Router();
 const productsController = require("../controllers/products");
-router.get("/",productsController.getProducts);
-
+router.get("/", productsController.getProducts);
 
 module.exports = router;
