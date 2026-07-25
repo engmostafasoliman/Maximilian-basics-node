bf0a332436880c1ce691af5d524792dfeb1d8487
Mostafa Soliman <engmostafasoliman@gmail.com>
2026-07-25 23:10:57 +0400

    feat: update navigation, routes, and data to include product listing and cart pages

--- routes/shop.js
+++ routes/shop.js
@@ -2,5 +2,8 @@ const express = require("express");
 const router = express.Router();
 const productsController = require("../controllers/products");
 router.get("/", productsController.getProducts);
+router.get("/products", productsController.getProducts);
+router.get("/cart", productsController.getProducts);
+router.get("/checkout", productsController.getProducts);
 
 module.exports = router;
