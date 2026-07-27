204edc6487a7b4ef1ca3c60deef2624bdff34255
Administrator <engmostafasoliman0@gmail.com>
2026-07-27 14:31:51 +0400

    feat: implement shop controller and product model with JSON data storage

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,9 +13,9 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
     console.log(prodId);
-    /*Product.fetchAll((products) => {
-        res.render("shop/product-detail", { prods: products, pageTitle: "Shop", path: "/", });
-    });*/
+    Product.findById(prodId,(product) => {
+       console.log(product);
+    });
 }
 //get index
 exports.getIndex = (req, res, next) => {
