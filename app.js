cff09c0fb8a9d005ca793cfd984e4e0cf69a6e04
Mostafa Soliman <engmostafasoliman0@gmail.com>
2026-07-31 16:45:55 +0400

    feat: implement Sequelize ORM for database management and product model integration

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-//const expressHbs = require("express-handlebars");
 const app = express();
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const path = require("path");
 const errorController = require("./controllers/errors");
+const sequelize = require("./util/database");
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,"public")));
-//app.engine("hbs",expressHbs({layoutsDir:"views/layouts/",defaultLayout:"main-layout",extname:"hbs"}));
+
 app.set("view engine","ejs");
 app.set("views","views");
     
 
 app.use("/admin",adminRoutes);
+
 app.use(shopRoutes);
-
-
 app.use(errorController.get404);
-app.listen(3001); 
+
+ sequelize.sync().then((result)=>{
+    // console.log(result); 
+    app.listen(3001); 
+ }).catch((err)=>{
+    console.log(err);
+ }); 
+
+
 
 module.exports = app;
 
