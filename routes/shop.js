97c5821c72548f5acb8ffb07a0387aa26a5185f2
Mostafa Soliman <engmostafasoliman0@gmail.com>
2026-08-03 13:20:24 +0400

    chore: install project dependencies and generate node_modules directory

--- routes/shop.js
+++ routes/shop.js
@@ -9,6 +9,6 @@ router.get("/orders", shopController.getOrders);
 router.get("/products/:productId", shopController.getProduct);
 router.post("/cart", shopController.postCart);
 router.post("/cart-delete-item",shopController.postCartDeleteItem);
-
+router.post("/create-order",shopController.postOrder);
 
 module.exports = router;
