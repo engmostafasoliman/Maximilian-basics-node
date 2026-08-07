c2be3d927ee0652902cafb7e4b1a688b4e0a8ff6
Mostafa Soliman <engmostafasoliman0@gmail.com>
2026-08-07 15:50:21 +0400

    fix: correct mongoConnect import and re-enable add-product route
    - Import mongoClient function from util/database instead of the module object
    - Uncomment GET /admin/add-product route so the page renders instead of 404
    - Continue MongoDB migration in products/order models and admin controller

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const path = require("path");
 const errorController = require("./controllers/errors");
-const mongoConnect = require("./util/database");
-const Product = require("./models/products");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cat-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+const mongoConnect = require("./util/database").mongoClient;
+const { MongoClient } = require("mongodb");
+// const Product = require("./models/products");
+// const User = require("./models/user");
+// const Cart = require("./models/cart");
+// const CartItem = require("./models/cat-item");
+// const Order = require("./models/order");
+// const OrderItem = require("./models/order-item");
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,"public")));
 
@@ -19,56 +20,20 @@ app.set("view engine","ejs");
 app.set("views","views");
 
 app.use((req,res,next)=>{
-    User.findByPk(1).then((user)=>{
-        req.user = user;
-        next();
-    }).catch((err)=>{
-        console.log(err);
-    });
+    // User.findByPk(1).then((user)=>{
+    //     req.user = user;
+    //     next();
+    // }).catch((err)=>{
+    //     console.log(err);
+    // });
+    next();
 });
 app.use("/admin",adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
-mongoConnect((client)=>{
-    console.log(client);
+mongoConnect(()=>{    
     app.listen(3001); 
 });
-// Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
-// User.hasMany(Product);
-// Cart.belongsTo(User);
-// User.hasOne(Cart);
-// Cart.belongsToMany(Product, { through: CartItem });
-// Product.belongsToMany(Cart, { through: CartItem });
-// Order.belongsTo(User);
-// User.hasMany(Order);
-// Order.belongsToMany(Product, { through: OrderItem });
-// Product.belongsToMany(Order, { through: OrderItem });
-
-
-// sequelize.sync().then((result)=>{
-//     // console.log(result); 
-//     return User.findByPk(1);
-//  }).then((user)=>{
-//     if(!user){
-//         return User.create({name:"Mostafa Soliman",email:"developer@easytouchuae.ae",password:"123456"});
-//     }
-//     return user;
-//  }).then((user)=>{
-//     user.getCart().then((cart)=>{
-//         if(!cart){
-//             return user.createCart();
-//         }
-//         return cart;
-//     }).then((cart)=>{
-//         console.log("cart is : ",cart);
-//     });
-//     console.log("user is : ",user);
-//    
-//  }).catch((err)=>{
-//     console.log(err);
-//  }); 
-
-
 
 module.exports = app;
 
--- models/order.js
+++ models/order.js
@@ -1,16 +1,16 @@
-const Sequelize = require("sequelize");
-const sequelize = require("../util/database");
-const Order = sequelize.define(
-    "order",
-    {
-        id:{
-            type:Sequelize.INTEGER,
-            autoIncrement:true,
-            allowNull:false,
-            primaryKey:true,
-            unique:true
-        }
-    }
-)
+// const Sequelize = require("sequelize");
+// const sequelize = require("../util/database");
+// const Order = sequelize.define(
+//     "order",
+//     {
+//         id:{
+//             type:Sequelize.INTEGER,
+//             autoIncrement:true,
+//             allowNull:false,
+//             primaryKey:true,
+//             unique:true
+//         }
+//     }
+// )
 
-module.exports = Order;
+// module.exports = Order;
