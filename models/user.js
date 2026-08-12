9b1c37de750a3d32d2b21b4ef923f0ef8b80dfb2
Mostafa Soliman <engmostafasoliman0@gmail.com>
2026-08-12 11:34:15 +0400

    fix: use _id in edit-product form and continue user/product MongoDB migration
    - edit-product.ejs hidden field now reads product._id (valid ObjectId) instead of product.id, fixing the BSONError on update
    - Wire user-loading middleware and continue migrating user/product models and admin controller to MongoDB

--- models/user.js
+++ models/user.js
@@ -1,28 +1,64 @@
-const Sequelize = require("sequelize");
-const sequelize = require("../util/database");
-const User = sequelize.define(
-    "user",
-    {
-        id:{
-            type:Sequelize.INTEGER,
-            autoIncrement:true,
-            allowNull:false,
-            primaryKey:true,
-            unique:true
-        },
-        name:{
-            type:Sequelize.STRING,
-            allowNull:false
-        },
-        email:{
-            type:Sequelize.STRING,
-            allowNull:false,
-            unique:true
-        },
-        password:{
-            type:Sequelize.STRING,
-            allowNull:false
-        }
+const getDb = require("../util/database").getDb;
+const ObjectId = require("mongodb").ObjectId;
+class User {
+    constructor(
+        username,email,
+    ){
+        this.name = username;
+        this.email = email;
+        
     }
-)
+    save(){
+        const db =getDb;
+        db.collection.insertOne(this).then((result) => {
+            console.log("user created",result);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+    static findById(id){
+        const db = getDb();
+        return db.collection("users").findOne({_id: new ObjectId(id)}).then((user) => {
+            return user;
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+    
+}
 module.exports = User;
+
+
+
+
+
+
+
+// const Sequelize = require("sequelize");
+// const sequelize = require("../util/database");
+// const User = sequelize.define(
+//     "user",
+//     {
+//         id:{
+//             type:Sequelize.INTEGER,
+//             autoIncrement:true,
+//             allowNull:false,
+//             primaryKey:true,
+//             unique:true
+//         },
+//         name:{
+//             type:Sequelize.STRING,
+//             allowNull:false
+//         },
+//         email:{
+//             type:Sequelize.STRING,
+//             allowNull:false,
+//             unique:true
+//         },
+//         password:{
+//             type:Sequelize.STRING,
+//             allowNull:false
+//         }
+//     }
+// )
+// module.exports = User;
