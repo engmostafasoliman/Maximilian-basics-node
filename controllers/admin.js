3325d6b7fbce2d71c86c5cb4728f870693c02cea
Mostafa Soliman <engmostafasoliman0@gmail.com>
2026-08-17 10:39:46 +0400

    feat: migrate data models and controllers to use Mongoose schemas

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,7 +11,7 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageurl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(title,imageUrl,price,description,null,req.user._id);
+    const product = new Product({title :title ,imageUrl:imageUrl,price:price,description:description,});
     product.save().then((result) => {
         console.log("created");
         res.redirect("/admin/products"); 
@@ -36,7 +36,6 @@ exports.postEditProduct = (req, res, next) => {
     const updatedDescription = req.body.description;
     const product = new Product(updatedTitle,updatedImageUrl,updatedPrice,updatedDescription ,prodId);
     product.save().then(() => {
-        console.log("updated");
         res.redirect("/admin/products");
     }).catch((err) => {
         console.log(err);
