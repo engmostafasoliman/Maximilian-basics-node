2aeabe217d411ecb66dc6840590e04ea0b4d1ad7
Mostafa Soliman <engmostafasoliman0@gmail.com>
2026-08-20 17:59:37 +0400

    236. Manipulating Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,9 @@
 exports.getLogin = (req, res, next) => {
-const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1];
-    res.render("auth/login", { pageTitle: "Login", path: "/login",isAuthenticated:isLoggedIn });
+// const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
+    res.render("auth/login", { pageTitle: "Login", path: "/login",isAuthenticated:false });
 }
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader("Set-Cookie", "loggedIn=true");
+    res.setHeader("Set-Cookie", "loggedIn=true;HttpOnly");
     res.redirect("/");
 }
