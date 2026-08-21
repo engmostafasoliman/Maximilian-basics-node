7a08fce20c28b02315606310ff7fbbac550a5fb8
Mostafa Soliman <engmostafasoliman0@gmail.com>
2026-08-21 10:46:30 +0400

    feat: implement session management using express-session and connect-mongodb-session with a custom database utility

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,10 @@
 exports.getLogin = (req, res, next) => {
 // const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
-    res.render("auth/login", { pageTitle: "Login", path: "/login",isAuthenticated:false });
+    console.log(req.session.isLoggedIn);
+    res.render("auth/login", { pageTitle: "Login", path: "/login",isAuthenticated:req.session.isLoggedIn });
 }
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader("Set-Cookie", "loggedIn=true;HttpOnly");
+    req.session.isLoggedIn = true;
     res.redirect("/");
 }
